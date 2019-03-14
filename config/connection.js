102a8ee69c9600d3efb21230707fb374b5fb5a7e
mouhoub bouhoum <mouhoub@mouhoubs-MacBook-Air.local>
2019-03-13 19:32:55 -0500

    express handlebars

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,9 @@
 let mysql = require("mysql");
 let connection;
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -8,6 +11,7 @@ connection = mysql.createConnection({
     password: "MB@mb20031988",
     database: "burger_db"
 });
+};
 
 connection.connect( (err) => {
     if (err) throw err;
